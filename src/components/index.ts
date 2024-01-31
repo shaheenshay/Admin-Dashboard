8db58513cd25421e55436cb0c3fe215a9d963042
shaheenshay <shaheen.shay@gmail.com>
2024-02-01 00:53:17 +0530

    upcoming events

---
+++ src/components/index.ts
@@ -0,0 +1,10 @@
+import UpcomingEvents from "./home/upcoming-events";
+import DealsChart from "./home/deals-chart";
+import UpcomingEventsSkeleton from "./skeleton/upcoming-events";
+import KanbanColumnSkeleton from "./skeleton/kanban";
+import LatestActivitiesSkeleton from "./skeleton/latest-activities";
+import AccordionHeaderSkeleton from "./skeleton/accordion-header";
+import ProjectCardSkeleton from "./skeleton/project-card";
+
+
+export { UpcomingEvents, DealsChart, UpcomingEventsSkeleton, ProjectCardSkeleton, LatestActivitiesSkeleton, KanbanColumnSkeleton, AccordionHeaderSkeleton };
