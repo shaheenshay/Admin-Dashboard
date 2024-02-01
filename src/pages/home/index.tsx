b2e47cb37f155d29d516dfb045bae367963a68a8
shaheenshay <shaheen.shay@gmail.com>
2024-02-01 23:49:07 +0530

    count cards added

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -4,6 +4,19 @@ import { Col, Row } from "antd"
 export const Home = () => {
   return (
     <div>
+      <Row gutter={[32, 32]}>
+        <Col xs={24} sm={24} xl={8}>
+          DashBoardTotalCountCard
+        </Col>
+        <Col xs={24} sm={24} xl={8}>
+          DashBoardTotalCountCard
+        </Col>
+        <Col xs={24} sm={24} xl={8}>
+          DashBoardTotalCountCard
+        </Col>
+      </Row>
+
+
       <Row
         gutter={[32, 32]}
         style={{
